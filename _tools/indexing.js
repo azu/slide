445324de1e799d62b65c85e9567183fc0ffc934c
azu <azuciao@gmail.com>
2014-07-09 22:52:03 +0900

    feat(tool): add indnexing tool

--- _tools/indexing.js
+++ _tools/indexing.js
@@ -7,7 +7,8 @@ var fs = require("fs");
 var pather = require("path");
 var FS = require("q-io/fs");
 var Promise = require("q");
-
+var getTitle = require("get-html-title");
+var Handlebars = require("handlebars");
 function pLoadFileList(fileList) {
     return Promise.all(fileList.map(function (filePath) {
         return FS.read(pather.resolve(filePath)).then(JSON.parse).catch(function (error) {
@@ -15,24 +16,57 @@ function pLoadFileList(fileList) {
         });
     }));
 }
-var concatPromise = FS.listTree("../", function isIndexHTML(filePath, stat) {
+var rootDir = "../";
+var concatPromise = FS.listTree(rootDir, function isIndexHTML(filePath, stat) {
     if (stat.isDirectory()) {
         return false;
     }
 
     // ignore rule
     var ignoreRule = /(reveal\.js|landslide-theme|node_modules)/;
-    if(ignoreRule.test(filePath)){
+    if (ignoreRule.test(filePath)) {
         return false;
     }
 
     return pather.extname(filePath) === ".html";
-}).then(function (fileList) {
-    return fileList
 });
 
-concatPromise.then(function (array) {
-    console.log(array);
-}).then(function (result) {
-    console.log("All Finish");
+function getTitleFromHTMLPath(filePath) {
+    var content = fs.readFileSync(filePath, "utf-8");
+    return getTitle(content);
+}
+// ディレクトリレベルでグルーピングする
+/*
+    "dir" : [
+        file...
+    ]
+ */
+function groupingFileList(fileList) {
+    var listObject = {};
+    fileList.forEach(function (filePath) {
+        var keyName = pather.dirname(filePath).split(pather.sep).pop();
+        if (listObject[keyName] == null) {
+            listObject[keyName] = [];
+        }
+        listObject[keyName].push({
+            filePath: pather.relative(rootDir, filePath),
+            title: getTitleFromHTMLPath(filePath)
+        });
+    });
+    return listObject;
+}
+Handlebars.registerHelper('link_to', function () {
+    return new Handlebars.SafeString("<a href='" + Handlebars.Utils.escapeExpression(this.filePath) + "'>" + Handlebars.Utils.escapeExpression(this.title) + "</a>");
 });
+function compileToHTML(fileListObject) {
+    var context = { object: fileListObject };
+    var source = fs.readFileSync("./index-template.hbs", "utf-8");
+    var template = Handlebars.compile(source);
+    return template(context);
+}
+var fileListObject = concatPromise.then(groupingFileList);
+fileListObject.then(function (result) {
+    var generatedHTML = compileToHTML(result);
+
+    return fs.writeFileSync(pather.resolve(rootDir, "./index.html"), generatedHTML);
+}).catch(console.error.bind(console));
